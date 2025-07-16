32098c041c457891ded8593d03396b66acee613c
Anik5284 <anikb5016@gmail.com>
2025-07-16 22:32:36 +0530

    16 th commit

---
+++ app/api/reset-password/route.ts
@@ -0,0 +1,26 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+
+export const POST = async (request: any) => {
+    const { password, email } = await request.json();
+
+    await connect();
+
+    const existingUser = await User.findOne({ email });
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+    existingUser.password = hashedPassword;
+
+    existingUser.resetToken = undefined;
+    existingUser.resetTokenExpiry = undefined;
+
+    try{
+        await existingUser.save();
+        return new NextResponse("User password is updated ", { status: 200});
+    } catch(err: any){
+        return new NextResponse(err, {status: 500});
+    }
+};
