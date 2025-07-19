c7dd89ebfc35e7a2192adf6e1b2e10c60a27e975
Anik5284 <anikb5016@gmail.com>
2025-07-19 14:06:39 +0530

    28  commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -116,7 +116,7 @@ export default function GovernmentPortal() {
                 </Card>
               </Link>
 
-              <Link href="/locker" className="block h-full">
+              <Link href="/digitallocker" className="block h-full">
                 <Card className="hover:shadow-lg group transition cursor-pointer h-full">
                   <CardContent className="p-6 text-center h-full flex flex-col justify-center">
                     <div className="bg-purple-100 text-purple-600 rounded-full p-4 w-16 h-16 mx-auto mb-4 group-hover:bg-purple-600 group-hover:text-white">
